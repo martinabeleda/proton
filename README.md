eee0f68b34b3d461907f4640befc37c9279da952
martinabeleda <martin.abeleda@gmail.com>
2023-04-07 21:36:24 -0700

    Add model config

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ A rust based service for serving machine learning predictions
 
 ## Testing
 
+Start the service
+
 ```shell
 cargo run
 ```
 
+Make a prediction
+
 ```shell
-curl -X POST -H "Content-Type: application/json" -d '{"model_type": "onnx", "model_path": "/path/to/your/model.onnx", "input_data": [0.0, 1.0, 2.0]}' http://localhost:8080/infer
+curl -X POST -H "Content-Type: application/json" -d '{"model_name": "squeezenet", "input_data": [0.0, 1.0, 2.0]}' http://localhost:8080/infer
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,48 @@
 pub mod backend;
 
-use axum::{
-    routing::post,
-    http::StatusCode,
-    response::IntoResponse,
-    Router, Json
-};
+use axum::extract::Extension;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::post;
+use axum::{Json, Router};
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::error::Error;
 use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::fs;
 
 use crate::backend::onnx::infer_onnx_model;
 
+#[derive(Debug, Deserialize)]
+struct ModelConfig {
+    name: String,
+    path: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    models: Vec<ModelConfig>,
+}
+
 #[derive(Deserialize)]
 struct InferenceRequest {
-    model_type: String,
-    model_path: String,
+    model_name: String,
     input_data: Vec<f32>,
 }
 
-async fn handle_inference(Json(info): Json<InferenceRequest>) -> impl IntoResponse {
+async fn load_config() -> Result<Config, Box<dyn Error>> {
+    let config_data = fs::read_to_string("config.yaml").await?;
+    let config: Config = serde_yaml::from_str(&config_data)?;
+    Ok(config)
+}
+
+async fn handle_inference(
+    Extension(models): Extension<Arc<HashMap<String, String>>>,
+    Json(info): Json<InferenceRequest>,
+) -> impl IntoResponse {
+    println!("Inference: {:?}", models);
+
     let output = infer_onnx_model().await;
 
     (StatusCode::OK, "Hello World!")
@@ -26,8 +50,20 @@ async fn handle_inference(Json(info): Json<InferenceRequest>) -> impl IntoRespon
 
 #[tokio::main]
 async fn main() {
+    let config = load_config().await.unwrap();
+    println!("Loaded config: {:?}", config);
+
+    let models: HashMap<String, String> = config
+        .models
+        .into_iter()
+        .map(|model_config| (model_config.name, model_config.path))
+        .collect();
+
+    let models = Arc::new(models);
+
     let app = Router::new()
-        .route("/infer", post(handle_inference));
+        .route("/infer", post(handle_inference))
+        .layer(Extension(models));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
