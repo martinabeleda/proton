c1bec8b186e1319a89abc714a8d6edb8bc480ad9
martinabeleda <martin.abeleda@gmail.com>
2023-04-10 19:15:26 -0700

    Add concurrent requests and timing info to client.rs

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,11 @@
 use ndarray::{Array, Array4};
 use reqwest::{Client, StatusCode};
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
+use tokio::task::JoinHandle;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 struct InferenceRequest {
     model_name: String,
     data: Array4<f32>,
@@ -13,6 +15,30 @@ struct InferenceRequest {
 struct InferenceResponse {
     prediction_id: Uuid,
     data: Vec<f32>,
+}
+
+async fn send_request(client: &Client, data: &InferenceRequest) -> Duration {
+    let start_time = tokio::time::Instant::now();
+    let response = client
+        .post("http://localhost:8080/predict")
+        .json(&data)
+        .send()
+        .await
+        .unwrap();
+
+    match response.status() {
+        StatusCode::OK => {
+            match response.json::<InferenceResponse>().await {
+                Ok(parsed) => tracing::info!("Success {:?}", parsed.prediction_id),
+                Err(_) => tracing::error!("The response didn't match InferenceResponse"),
+            };
+        }
+        _ => {
+            tracing::error!("Something went wrong {:?}", response);
+        }
+    }
+
+    start_time.elapsed()
 }
 
 #[tokio::main]
@@ -41,22 +67,33 @@ async fn main() {
     };
 
     let client = Client::new();
-    let response = client
-        .post("http://localhost:8080/predict")
-        .json(&data)
-        .send()
-        .await
-        .unwrap();
 
-    match response.status() {
-        StatusCode::OK => {
-            match response.json::<InferenceResponse>().await {
-                Ok(parsed) => tracing::info!("Success {:?}", parsed),
-                Err(_) => tracing::error!("The response didn't match InferenceResponse"),
-            };
-        }
-        _ => {
-            tracing::error!("Something went wrong {:?}", response);
-        }
+    let num_requests = 100; // number of concurrent requests
+    let mut tasks: Vec<JoinHandle<Duration>> = Vec::with_capacity(num_requests);
+
+    for _ in 0..num_requests {
+        let data = data.clone();
+        let client = client.clone();
+        let task = tokio::spawn(async move { send_request(&client, &data).await });
+        tasks.push(task);
     }
+
+    let mut elapsed_times = Vec::with_capacity(num_requests);
+    for task in tasks {
+        elapsed_times.push(task.await.unwrap());
+    }
+
+    elapsed_times.sort_unstable();
+
+    let sum = elapsed_times.iter().cloned().sum::<Duration>();
+    let average = sum / num_requests as u32;
+
+    tracing::info!("Elapsed times: {:?}", elapsed_times);
+
+    let p95 = elapsed_times[((0.95 * (num_requests as f64)).round() as usize) - 1];
+    let p99 = elapsed_times[((0.99 * (num_requests as f64)).round() as usize) - 1];
+
+    tracing::info!("Average time: {:?}", average);
+    tracing::info!("p95 time: {:?}", p95);
+    tracing::info!("p99 time: {:?}", p99);
 }
